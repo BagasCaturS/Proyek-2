9dca19d0f1f55263ca9ff31e3d01a420f5e0d891
BagasCaturS <bagascatursantoso@gmail.com>
2023-05-08 10:39:17 +0700

    memperbaiki crawling menjadi seperti di youtube

--- crawl/crawlingproject/crawlingproject/spiders/crawl.py
+++ crawl/crawlingproject/crawlingproject/spiders/crawl.py
@@ -3,25 +3,27 @@ from scrapy.linkextractors import LinkExtractor
 
 class CrawlingSpider(CrawlSpider):
     name = "mulai"
-    # allowed_domains = ["toscrape.com"]
-    # start_urls = ["https://books.toscrape.com/"]
-    allowed_domains = ["www.timeshighereducation.com"]
-    start_urls = ["https://www.timeshighereducation.com/world-university-rankings/2023/"]
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
+    # allowed_domains = ["https://telkomuniversity.ac.id"]
+    # start_urls = ["https://telkomuniversity.ac.id"]
 
     rules = (
-        # Rule(LinkExtractor(allow="catalogue/category")),
-        # Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item")
-        Rule(LinkExtractor(allow="world-ranking#!"), callback="parse_item"),
+        Rule(LinkExtractor(allow="catalogue/category")),
+        # Rule(LinkExtractor(allow="change", deny="category"), callback="parse_item")
+        Rule(LinkExtractor(allow="catalogue", deny="category") , callback="parse_item")
+        # Rule(LinkExtractor(allow="semua-berita-terkini"), callback="parse_item"),
         # yang dimaskdu dinsin adalah bagaimana pattern yang akan digunakan saat aakan mencsrape web/crawl
+        # THE ga bisa sd scrape; di proteksi
     )
 
-    # def parse_item(self, response):
-    #     yield {
-    #         "title": response.css(".product_main h1::text").get(),
-    #         "price": response.css(".price_color::text").get(),
-    #         "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ", "").replace("Instock", "").replace("available", "").replace("(", "").replace(")", "")
-    #     } 
     def parse_item(self, response):
         yield {
-            "title": response.css(".ranking-institution-title::text").get()
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".price_color::text").get(),
+            "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ", "").replace("Instock", "").replace("available", "").replace("(", "").replace(")", "")
         } 
+    # def parse_item(self, response):
+    #     yield {
+    #         "title": response.css(".quote::text").get()
+    #     } 
